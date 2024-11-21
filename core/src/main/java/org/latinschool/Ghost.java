a40beafb579798135f6e2a5a4b5e00ae4a69be60
Carolena Tognarelli <ctognare@lsoc.org>
2024-11-21 09:51:37 -0600

    Worked on Ghost Movement
    Worked on Ghost Movement

---
+++ core/src/main/java/org/latinschool/Ghost.java
@@ -0,0 +1,49 @@
+package org.latinschool;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+
+public class Ghost extends Sprite {
+    int ghostX;
+    int ghostY;
+    MyPacManGame game;
+
+    private float ghostMoveTime = 0.06f; // 0.5 seconds pause (adjustable)
+    private float ghostMoveTimer = 0f;
+
+    Direction direction;
+    public Ghost(Direction startingDirection, MyPacManGame game) {
+        super();
+        this.direction = startingDirection;
+        this.ghostX = 24;
+        this.ghostY = 24;
+        this.game = game;
+
+    }
+    public Ghost(Texture texture, Direction startingDirection, int ghostX, int ghostY, MyPacManGame game) {
+        super(texture);
+        this.direction = startingDirection;
+        this.ghostX = ghostX;
+        this.ghostY = ghostY;
+        this.game = game;
+    }
+    /*public void move(){
+        float delta = Gdx.graphics.getDeltaTime();
+        // Only allow movement if the ghost move timer has finished
+        if (ghostMoveTimer > 0) {
+            ghostMoveTimer -= delta;
+            return;
+        }
+        // Get current position in tiles
+        int tileX = (int) (ghostX / wallLayer.getTileWidth());
+        int tileY = (int) (ghostY / wallLayer.getTileHeight());
+
+        //switch (direction) {
+          //  case UP:
+                //if (!isCo)
+       // }
+    }*/
+
+}
--- core/src/main/java/org/latinschool/MyPacManGame.java
+++ core/src/main/java/org/latinschool/MyPacManGame.java
@@ -15,6 +15,8 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
+import static java.lang.Thread.sleep;
+
 
 public class MyPacManGame implements ApplicationListener {
     Texture backgroundTexture;
@@ -40,9 +42,15 @@ public class MyPacManGame implements ApplicationListener {
     private TiledMapTileLayer wallLayer;
     private TiledMapTileLayer pelletLayer;// Track pellet timers
 
+    private float pauseTime = 0.06f; // 0.5 seconds pause (adjustable)
+    private float pauseTimer = 0f;
+    private float ghostMoveTime = 0.06f; // 0.5 seconds pause (adjustable)
+    private float ghostMoveTimer = 0f;
 
     Pixmap mapPixmap;
     Color wallColor;
+
+    private float ghostX, ghostY;
 
     private OrthogonalTiledMapRenderer tiledMapRenderer;
 
@@ -56,15 +64,19 @@ public class MyPacManGame implements ApplicationListener {
         viewport.getCamera().position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
         viewport.getCamera().update();
 
-        ghostSprite = new Sprite(ghostTexture);
+        //ghostSprite = new Sprite(ghostTexture);
+        ghostSprite = new Ghost(ghostTexture, Direction.RIGHT, 24,16,this);
         pacmanSprite = new Sprite(pacmanTexture);
 
         ghostSprite.setSize(8, 8);
         pacmanSprite.setSize(8, 8);
 
 
-        ghostSprite.setPosition(20, 20);
-        pacmanSprite.setPosition(16, 16);
+        ghostSprite.setPosition(16, 32);
+        pacmanSprite.setPosition(16, 24);
+
+        ghostY = 24;
+        ghostX = 24;
 
         spriteBatch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
@@ -96,6 +108,7 @@ public class MyPacManGame implements ApplicationListener {
     @Override
     public void render() {
         input();
+       // ghostSprite.move();
         logic();
         points();
         draw();
@@ -112,50 +125,50 @@ public class MyPacManGame implements ApplicationListener {
     }
 
     public void input() {
-        float delta = Gdx.graphics.getDeltaTime();
+            float delta = Gdx.graphics.getDeltaTime();
 
-        int speed = 40;
-
-        // Pac-Man's current position
-        float pacmanX = pacmanSprite.getX();
-        float pacmanY = pacmanSprite.getY();
-
-        // Get the current tile coordinates (based on Pac-Man's current position)
-        int tileX = (int) (pacmanX / wallLayer.getTileWidth());
-        int tileY = (int) (pacmanY / wallLayer.getTileHeight());
-        int yPix = tileY * wallLayer.getTileWidth();
-        int xPix = tileX * wallLayer.getTileWidth();
-
-
-        System.out.println(tileX + ", " + tileY);
-
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            if (!isBarrierTile(tileX + 1, tileY)) {
-                pacmanSprite.setPosition((tileX + 1)*8, yPix);
-                //pacmanSprite.translateX(speed * delta);  // Move right
+            // Only allow movement if the pauseTimer is finished
+            if (pauseTimer > 0) {
+                pauseTimer -= delta;
+                return; // If the pauseTimer is still running, don't allow movement
             }
-        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            // Check for collision with the tile to the left
-            if (!isBarrierTile(tileX - 1, tileY)) {
-                pacmanSprite.setPosition((tileX - 1)*8, yPix);
-                //pacmanSprite.translateX(speed * -delta);  // Move left
-            }
-        } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            if (!isBarrierTile(tileX, tileY + 1)) {
-                pacmanSprite.setPosition(xPix, (tileY + 1)*8);
-                //pacmanSprite.translateY(speed * delta);
-                System.out.println("You can move up!");
-            }
-            else{
-                System.out.println("You can't move up!");
-            }
-        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            if (!isBarrierTile(tileX, tileY - 1)) {
-                pacmanSprite.setPosition(xPix, (tileY - 1)*8);
-                //pacmanSprite.translateY(speed * -delta);
+
+            // Pac-Man's current position
+            float pacmanX = pacmanSprite.getX();
+            float pacmanY = pacmanSprite.getY();
+
+            // Get the current tile coordinates
+            int tileX = (int) (pacmanX / wallLayer.getTileWidth());
+            int tileY = (int) (pacmanY / wallLayer.getTileHeight());
+            int yPix = tileY * wallLayer.getTileWidth();
+            int xPix = tileX * wallLayer.getTileWidth();
+
+            System.out.println(tileX + ", " + tileY);
+
+
+            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+                if (!isBarrierTile(tileX + 1, tileY)) {
+                    pacmanSprite.setPosition((tileX + 1) * 8, yPix);
+                    pauseTimer = pauseTime;
+
+                }
+            } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+                if (!isBarrierTile(tileX - 1, tileY)) {
+                    pacmanSprite.setPosition((tileX - 1) * 8, yPix);
+                    pauseTimer = pauseTime;
+                }
+            } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+                if (!isBarrierTile(tileX, tileY + 1)) {
+                    pacmanSprite.setPosition(xPix, (tileY + 1) * 8);
+                    pauseTimer = pauseTime;
+                }
+            } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+                if (!isBarrierTile(tileX, tileY - 1)) {
+                    pacmanSprite.setPosition(xPix, (tileY - 1) * 8);
+                    pauseTimer = pauseTime;
+                }
             }
         }
-    }
 
     public void logic() {
         float worldWidth = viewport.getWorldWidth();
@@ -200,6 +213,72 @@ public class MyPacManGame implements ApplicationListener {
             }
 
     }
+
+    public void moveGhost() {
+        float delta = Gdx.graphics.getDeltaTime();
+
+        // Only allow movement if the ghost move timer has finished
+        if (ghostMoveTimer > 0) {
+            ghostMoveTimer -= delta;
+            return;
+        }
+
+        // Random direction
+        int direction = MathUtils.random(3);
+
+        // Get current position in tiles
+        int tileX = (int) (ghostX / wallLayer.getTileWidth());
+        int tileY = (int) (ghostY / wallLayer.getTileHeight());
+
+
+        // Movement logic: check each direction
+        boolean moved = false;
+
+        switch (direction) {
+            case 0: // Move Up
+                if (!isBarrierTile(tileX, tileY + 1)) {
+                    ghostY += wallLayer.getTileHeight(); // Move the ghost up
+                    moved = true;
+                }
+                else {
+                    // If collided with a wall, change direction
+                    direction = MathUtils.random(3); // Choose a new random direction
+                }
+                break;
+            case 1: // Move Down
+                if (!isBarrierTile(tileX, tileY - 1)) {
+                    ghostY -= wallLayer.getTileHeight(); // Move the ghost down
+                    moved = true;
+                }
+                else {
+                    direction = MathUtils.random(3);
+                }
+                break;
+            case 2: // Move Left
+                if (!isBarrierTile(tileX - 1, tileY)) {
+                    ghostX -= wallLayer.getTileWidth(); // Move the ghost left
+                    moved = true;
+                }
+                else {
+                    direction = MathUtils.random(3);
+                }
+                break;
+            case 3: // Move Right
+                if (!isBarrierTile(tileX + 1, tileY)) {
+                    ghostX += wallLayer.getTileWidth(); // Move the ghost right
+                    moved = true;
+                }
+                else {
+                    direction = MathUtils.random(3);
+                }
+                break;
+        }
+
+        if (moved) {
+            ghostMoveTimer = ghostMoveTime; // Reset the move timer
+        }
+    }
+
     public boolean isBarrierTile(int tileX, int tileY) {
         // Get the tile at the given coordinates
         TiledMapTileLayer.Cell cell = wallLayer.getCell(tileX, tileY);
@@ -225,8 +304,9 @@ public class MyPacManGame implements ApplicationListener {
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
         spriteBatch.begin();
 
-        ghostSprite.draw(spriteBatch);
         pacmanSprite.draw(spriteBatch);
+        ghostSprite.setPosition(ghostX, ghostY);
+        ghostSprite.draw(spriteBatch);
 
 
         spriteBatch.end();
